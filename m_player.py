a922856166541e9def39a16c53f387281ccb315b
shaz <codebasileus@protonmail.com>
2017-10-12 17:03:31 -0400

    Switched storage from Python lists to VLC data structures. Refined various things

--- m_player.py
+++ m_player.py
@@ -3,56 +3,77 @@
 import curses
 import vlc
 
-songs = []
-files = []
-songPointer = -1
+active = 0
 
 # Screen operations
 stdscr = curses.initscr()
-screenH = stdscr.getmaxyx()[0]
-screenW = stdscr.getmaxyx()[1]
+scrH = stdscr.getmaxyx()[0]
+scrW = stdscr.getmaxyx()[1]
 
+# VLC operations
+files = vlc.MediaList()
+player = vlc.MediaListPlayer()
+player.set_media_list(files)
+
+def init_scr():
+    # Colors
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
+
+    # Add the title of the player
+    title = "M_PLAYER"
+    titleX = int(scrW/2) - int(len(title)/2)
+    stdscr.addstr(0, titleX, title, curses.color_pair(1))
+
+    # Add rest of UI
+    stdscr.addstr(1, 10, "Commands", curses.color_pair(1))
+    stdscr.addstr(3, 10, "$$ open <file> - open a media file", curses.A_BOLD)
+    stdscr.addstr(4, 10, "$$ play <num> - play media file at position num", curses.A_BOLD)
+    stdscr.addstr(3, 60, "$$ stop - stop playing media", curses.A_BOLD)
+    stdscr.addstr(4, 60, "$$ leave - exit program", curses.A_BOLD)
+    stdscr.addstr(6, 10, "Current files available", curses.color_pair(1))
+    
 def user_input():
-    stdscr.addstr(screenH-1, 0, "> ")
-    instream = stdscr.getstr(screenH-1,2,screenW-1).decode("utf-8")
-    stdscr.addstr(screenH-1, 2, " "*len(instream))
-    return instream.lower().strip()
+    stdscr.addstr(scrH-1, 0, "$$ ")
+    instream = stdscr.getstr(scrH-1,3,scrW-1).decode("utf-8")
+    stdscr.addstr(scrH-1, 3, " "*len(instream))
+    res = instream.lower().strip()
+    return res
 
-# Colors
-curses.start_color()
-curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
-curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
+def stop_media(active):
+    player.stop()
+    stdscr.addstr(active+7, 13+len(files[active].get_mrl()[33:]), " "*10)
 
-# Add the title of the player
-title = "M_PLAYER"
-titleX = int(screenW/2) - int(len(title)/2)
-stdscr.addstr(0, titleX, title, curses.color_pair(1))
+#def play_active():
+#    player.play()
 
-# Basic UI elements
-stdscr.addstr(1, 10, "Commands", curses.color_pair(1))
-stdscr.addstr(3, 10, "> open <file> - open an audio file", curses.A_BOLD)
-stdscr.addstr(4, 10, "> play - play most recently added audio file", curses.A_BOLD)
-stdscr.addstr(3, 60, "> stop - stop playing file", curses.A_BOLD)
-stdscr.addstr(4, 60, "> leave - exit program", curses.A_BOLD)
-stdscr.addstr(6, 10, "Current songs available", curses.color_pair(1))
+init_scr()
+action = user_input()
 
-# Ask for input
-command = user_input()
-while (command!="leave"):
-    if command[0:4] == "open":
-        active = vlc.MediaPlayer("/Users/ramesses/Downloads/"+command[5:])
-        songs.append(command[5:])
-        songPointer+=1
-        for i in range(len(songs)):
-            stdscr.addstr(i+7, 10, str(i+1) + ". " + songs[i], curses.color_pair(3)) 
-    elif command == "play":
-        active.play()
-        stdscr.addstr(songPointer+7, 10+len(songs[songPointer]), ": Playing", curses.color_pair(2))
-    elif command == "stop":
-        active.stop()
+# Main loop of program
+while (action != "leave"):
+    
+    if action[0:4] == "open":
+        path = ("/Users/ramesses/Downloads/"+action[5:])
+        files.add_media(path)
+        for i in range(len(files)):
+            stdscr.addstr(i+7, 10, str(i+1)+". "+files[i].get_mrl()[33:], curses.color_pair(3))
+
+    elif action[0:4] == "play":
+        stop_media(active)
+        active = int(action[5:])-1
+        player.play_item_at_index(active)
+        print(files[active].get_mrl()[33:])
+        print(len(files[active].get_mrl()[33:]))
+        stdscr.addstr(active+7, 13+len(files[active].get_mrl()[33:]), ": Playing", curses.color_pair(2))
+
+    elif action == "stop":
+        stop_media(active)
+
     stdscr.refresh()
-    command = user_input()
+    action = user_input()
 
 # Close window
 curses.endwin()
